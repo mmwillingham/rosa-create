763263dc5c6c756bb351ee9f038e0a05e2af70df
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-17 16:38:06 -0400

    .

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ locals {
 }
 
 provider "rhcs" {
-#  token = var.token
+  url   = var.url
+  token = var.token
 #  token = data.aws_secretsmanager_secret_version.secret_credentials.secret_string
 #  token = local.rosa-secrets
 #  token = local.rosa-secrets.ocm_token
-  token = var.OCM_TOKEN
-  url   = var.url
+#  token = var.OCM_TOKEN
 }
 
 locals {
