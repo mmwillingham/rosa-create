24eb16228b97c8b5a3ed722f47ff20f1a1630a2b
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-17 16:53:07 -0400

    .

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ locals {
 
 provider "rhcs" {
   url   = var.url
-  token = var.TOKEN
+  token = var.OCM_TOKEN
 #  token = data.aws_secretsmanager_secret_version.secret_credentials.secret_string
 #  token = local.rosa-secrets
 #  token = local.rosa-secrets.ocm_token
