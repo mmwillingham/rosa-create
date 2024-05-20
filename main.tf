44691e727e4ebedd60c12c450fd54721986e8915
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-20 13:54:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,16 +79,13 @@ resource "rhcs_cluster_rosa_classic" "rosa_sts_cluster" {
   multi_az           = var.multi_az
   availability_zones = var.availability_zones
   replicas           = var.replicas
-  properties = {
-    rosa_creator_arn = data.aws_caller_identity.current.arn
-  }
+  properties = { rosa_creator_arn = data.aws_caller_identity.current.arn }
   sts                = local.sts_roles  
   destroy_timeout    = 60  
   depends_on         = [time_sleep.wait_for_roles]
   version            = var.openshift_version
   admin_credentials  = {
-#     password        = var.ADMIN_PASSWORD
-      password        = var.ADMIN_PASSWORD
+     password        = var.ADMIN_PASSWORD
      username        = var.admin_username 
   }
   upgrade_acknowledgements_for = var.upgrade_acknowledgements_for
